be50a5aff241d136e8a11881f016ac5213ea25a4
Alexis Bonal <bonal.alexis@gmail.com>
2024-07-25 15:28:15 +0200

    Ajout de la fonction

--- src/ex1/index.ts
+++ src/ex1/index.ts
@@ -7,5 +7,26 @@ export function isRangeAvailable(
   requestedRange: DateRange,
   availableRange: DateRange
 ): boolean {
-  return false;
+  const requestStart = requestedRange.startDate.getTime();
+  const requestEnd = requestedRange.endDate.getTime();
+  const availableStart = availableRange.startDate.getTime();
+  const availableEnd = availableRange.endDate.getTime();
+
+  // Vérification de la plage demandée
+  if (requestStart > requestEnd) {
+    throw new Error("Les dates demandées sont erronées");
+  }
+
+  // Vérification de la plage disponible
+  if (availableStart > availableEnd) {
+    throw new Error("Les dates disponibles sont erronées");
+  }
+
+  // Vérification de l'égalité
+  return (
+    requestStart < availableStart &&
+    requestEnd > availableStart &&
+    requestStart > availableEnd &&
+    requestEnd > availableEnd
+  );
 }
